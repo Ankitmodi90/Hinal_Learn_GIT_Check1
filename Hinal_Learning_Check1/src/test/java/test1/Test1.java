f57582b20ffd2531babb111d804b94ad013e7033
admodi <admodi@ADMODI-LAP.oradev.oraclecorp.com>
2019-07-07 18:48:16 +0530

    Second change through Branch

--- Hinal_Learning_Check1/src/test/java/test1/Test1.java
+++ Hinal_Learning_Check1/src/test/java/test1/Test1.java
@@ -7,7 +7,7 @@ public class Test1 {
 	@Test
 	public void test()
 	{
-		System.out.println("Hinal is a Good Girl and Learn Automation");
+		System.out.println("Hinal is a Good Girl and Knows Automation");
 	}
 
 
